a0169258dc930b97654582d51f2640451d90d150
Angelo Ordonez <angelo-mjz7@hotmail.com>
2021-10-27 00:01:48 -0500

    implementacion de card y consumo de api de eventos

--- index.php
+++ index.php
@@ -15,20 +15,7 @@ include_once 'layout/navegacion.php';
 <!-- Contenedor card -->
 <div class="container-fluid py-5 bg-dark">
     <div class="container">
-        <div class="row g-3 d-flex justify-content-center" id="container-cards">
-            <div class="col-12 col-sm-6 col-lg-3">
-                <div class="card m-auto border-0">
-                    <img src="https://roman-company.com/TrailerMovilApiRest/images/245921356_876617609714123_6068865882472407514_n.jpg" class="card-img-top" style="max-height:300px; object-fit: cover;" alt="Mago de Oz">
-                    <div class="card-body text-center">
-                        <h3 class="card-title">Mago de Oz</h3>
-                        <p><i class="bi bi-currency-dollar"></i><strong>10</strong></p=>
-                        <p><i class="bi bi-calendargi-date"></i><strong> 26/10/2021</strong></p>
-                        <p><i class="bi bi-geo-alt-fill"></i><strong> Riobamba - Estadio</strong></p>
-                        <a href="#" class="btn btn-primary">Ver más</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="row g-3 d-flex justify-content-center justify-content-md-evenly" id="container-cards"> </div>
     </div>
 </div>
 <!-- Contenedor card />-->
---
+++ js/main.script.js
@@ -0,0 +1,53 @@
+/* -------------------------------------------------------------------------- */
+/*                          Variables globales e init                         */
+/* -------------------------------------------------------------------------- */
+
+initFunctions();
+
+/* -------------------------------------------------------------------------- */
+/*                             eventos de usuario                             */
+/* -------------------------------------------------------------------------- */
+
+// Esperamos a que cargue toda la página
+window.addEventListener('load', (event) => {
+
+});
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                            Funciones de usuario                            */
+/* -------------------------------------------------------------------------- */
+function initFunctions() {
+    getEventData();
+}
+
+async function getEventData() {
+    const fragment = document.createDocumentFragment();
+    const cardContainer = document.getElementById('container-cards');
+    const url = 'https://roman-company.com/TrailerMovilApiRest/view/evento.php?estado=active';
+    const request = await fetch(url);
+    const requestJson = await request.json();
+    const { datos: events } = requestJson;
+
+    events.forEach(event => {
+        const cardEvent = document.createElement('DIV');
+        cardEvent.classList.add('col-12','col-sm-6','col-md-5','col-lg-4');
+        cardEvent.innerHTML = `
+        <div class="card m-auto border-0">
+            <div class="image-wrapper-card">
+                <img src="${event.foto}" alt="${event.nombre}">
+            </div>
+            <div class="card-body text-center">
+                <h3 class="card-title">${event.nombre}</h3>
+                <p class="event-price-card"><i class="bi bi-currency-dollar"></i><strong>${event.precio}</strong></p=>
+                <p><i class="bi bi-calendar-date"></i> <strong>${event.fecha_evento.split(' ')[0]}</strong></p>
+                <p><i class="bi bi-geo-alt-fill"></i> <strong>${event.ubicacion}</strong></p>
+                <a href="#" class="btn btn-card">Ver más</a>
+            </div>
+        </div>
+        `;
+        fragment.appendChild(cardEvent);
+    });
+    cardContainer.appendChild(fragment);
+}
