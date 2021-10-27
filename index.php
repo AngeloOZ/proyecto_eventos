31c9d2d38cdbbbb64b9036c5610b515bcbb266e6
milewid <milewid@gmail.com>
2021-10-26 22:40:32 -0500

    se agrego estilos

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
     include_once 'layout/header.php';
     include_once 'layout/navegacion.php';    
 ?>
+
+<section class= "paralax">
+    <h1>Pagina de Eventos</h1>
+    <p>Ven y disfruta de los mejores eventos  </p>
+</section>
     
+
 <!-- hola -->
 
 <?php
