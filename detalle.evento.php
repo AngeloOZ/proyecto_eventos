7648bc9664bb79b2af8be3fa21468ef4aaabef8a
milewid <milewid@gmail.com>
2021-10-29 00:52:11 -0500

    nuevos cambios

---
+++ detalle.evento.php
@@ -0,0 +1,13 @@
+<?php
+  include_once 'layout/header.php';
+  include_once 'layout/navegacion.php';
+?>
+
+<div class="container ">
+    
+</div>
+
+
+<?php
+    include_once 'layout/footer.php';    
+?>
