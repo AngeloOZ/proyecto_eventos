b842789c76591b429e9a09d969cea90b3ca69428
Angelo Ordonez <angelo-mjz7@hotmail.com>
2021-10-30 23:37:42 -0500

    implementacion login

--- login.php
+++ login.php
@@ -2,82 +2,89 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/login_styles.css">
-  <title>Document</title>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="./css/login_styles.css" />
+   <title>Iniciar Sesión</title>
 </head>
 
 <body>
-
-  <div class="login-reg-panel">
-    <div class="login-info-box">
-      <h2>Have an account?</h2>
-      <p>Lorem ipsum dolor sit amet</p>
-      <label id="label-register" for="log-reg-show">Login</label>
-      <input type="radio" name="active-log-panel" id="log-reg-show" checked="checked">
-    </div>
-
-    <div class="register-info-box">
-      <h2>Don't have an account?</h2>
-      <p>Lorem ipsum dolor sit amet</p>
-      <label id="label-login" for="log-login-show">Register</label>
-      <input type="radio" name="active-log-panel" id="log-login-show">
-    </div>
-
-    <div class="white-panel">
-      <div class="login-show">
-        <h2>LOGIN</h2>
-        <input type="text" placeholder="Email">
-        <input type="password" placeholder="Password">
-        <input type="button" value="Login">
-        <a href="">Forgot password?</a>
+   <div class="container">
+      <div class="forms-container">
+         <div class="signin-signup">
+            <form action="#" class="sign-in-form">
+               <h2 class="title">Iniciar Sesión </h2>
+               <div class="input-field">
+                  <i class="fas fa-envelope"></i>
+                  <input type="email" placeholder="Email" />
+               </div>
+               <div class="input-field">
+                  <i class="fas fa-lock"></i>
+                  <input type="password" placeholder="Contraseña" />
+               </div>
+               <input type="submit" value="Login" class="btn solid" />
+            </form>
+            <form action="#" class="sign-up-form">
+               <h2 class="title">Registrate</h2>
+               <div class="input-field">
+                  <i class="fas fa-user"></i>
+                  <input type="text" placeholder="Nombres" />
+               </div>
+               <div class="input-field">
+                  <i class="fas fa-envelope"></i>
+                  <input type="email" placeholder="Email" />
+               </div>
+               <div class="input-field">
+                  <i class="fas fa-lock"></i>
+                  <input type="password" placeholder="Contraseña" />
+               </div>
+               <input type="submit" class="btn" value="Registrarse" />
+            </form>
+         </div>
       </div>
-      <div class="register-show">
-        <h2>REGISTER</h2>
-        <input type="text" placeholder="Email">
-        <input type="password" placeholder="Password">
-        <input type="password" placeholder="Confirm Password">
-        <input type="button" value="Register">
+
+      <div class="panels-container">
+         <div class="panel left-panel">
+            <div class="content">
+               <h3>Eres nuevo?</h3>
+               <p>
+                  Registrate en nuestra página para obtener más información acerca de los eventos que te podemos ofrecer
+               </p>
+               <button class="btn transparent" id="sign-up-btn">
+                  Registrarse
+               </button>
+            </div>
+            <!-- <img src="img/log.svg" class="image" alt="" /> -->
+         </div>
+         <div class="panel right-panel">
+            <div class="content">
+               <h3>Ya tienes cuenta?</h3>
+               <p>
+               Inicia sesión en nuestra página para obtener más información acerca de los eventos que te podemos ofrecer
+               </p>
+               <button class="btn transparent" id="sign-in-btn">
+                  Sign in
+               </button>
+            </div>
+            <!-- <img src="img/register.svg" class="image" alt="" /> -->
+         </div>
       </div>
-    </div>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script>
-   
-   $(document).ready(function(){
-    $('.login-info-box').fadeOut();
-    $('.login-show').addClass('show-log-panel');
-});
+   </div>
 
+   <script>
+      const sign_in_btn = document.querySelector("#sign-in-btn");
+      const sign_up_btn = document.querySelector("#sign-up-btn");
+      const container = document.querySelector(".container");
 
-$('.login-reg-panel input[type="radio"]').on('change', function() {
-    if($('#log-login-show').is(':checked')) {
-        $('.register-info-box').fadeOut(); 
-        $('.login-info-box').fadeIn();
-        
-        $('.white-panel').addClass('right-log');
-        $('.register-show').addClass('show-log-panel');
-        $('.login-show').removeClass('show-log-panel');
-        
-    }
-    else if($('#log-reg-show').is(':checked')) {
-        $('.register-info-box').fadeIn();
-        $('.login-info-box').fadeOut();
-        
-        $('.white-panel').removeClass('right-log');
-        
-        $('.login-show').addClass('show-log-panel');
-        $('.register-show').removeClass('show-log-panel');
-    }
-});
-  
-  </script>
+      sign_up_btn.addEventListener("click", () => {
+         container.classList.add("sign-up-mode");
+      });
+
+      sign_in_btn.addEventListener("click", () => {
+         container.classList.remove("sign-up-mode");
+      });
+   </script>
 </body>
 
 </html>
-<!------ Include the above in your HEAD tag ---------->
