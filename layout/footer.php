50fd319a0f41f81f28315e24d0a38e2b3891f20f
Angelo Ordonez <angelo-mjz7@hotmail.com>
2021-11-02 00:16:17 -0500

    update social media y cdn icons

--- layout/footer.php
+++ layout/footer.php
@@ -10,17 +10,11 @@
                 <!-- Twitter -->
                 <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="bi bi-twitter"></i></a>
 
-                <!-- Google -->
-                <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="bi bi-google"></i></a>
-
                 <!-- Instagram -->
                 <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="bi bi-instagram"></i></a>
 
-                <!-- Linkedin -->
-                <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="bi bi-linkedin"></i></a>
-
-                <!-- Github -->
-                <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="bi bi-github"></i></a>
+                <!-- Tiktok -->
+                <a class="btn btn-outline-light btn-floating m-1 rounded-circle" href="#!" role="button"><i class="bi bi-tiktok"></i></a>
             </section>
             <!-- Section: Social media -->
         </div>
