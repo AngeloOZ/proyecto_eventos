bd69cff65b7c0fa2ccf88f0e90c5fb6b6ebe0283
Angelo Ordonez <angelo-mjz7@hotmail.com>
2021-11-04 23:27:39 -0500

    refactory login

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="./css/login_styles.css" />
-    <script src="./js/jquery-3.6.0.js"></script>
    <title>Iniciar Sesión</title>
 </head>
 
@@ -14,7 +13,7 @@
    <div class="container">
       <div class="forms-container">
          <div class="signin-signup">
-            <form action="#" class="sign-in-form" autocomplete>
+            <form action="#" id="formIniciarSesion" class="sign-in-form" autocomplete>
                <h2 class="title">Iniciar Sesión </h2>
                <div class="input-field">
                   <i class="fas fa-envelope"></i>
@@ -24,7 +23,7 @@
                   <i class="fas fa-lock"></i>
                   <input type="password" id="pass_login" required placeholder="Contraseña" />
                </div>
-               <input type="submit" value="Login" class="btn solid" id="btn_login" />
+               <input type="submit" value="Iniciar Sesión" class="btn solid"/>
             </form>
             <form action="#" class="sign-up-form" id="r_formulario">
                <h2 class="title">Registrate</h2>
@@ -44,7 +43,7 @@
                     <i class="fas fa-phone-alt"></i>
                     <input id="r_tel" type="tel" placeholder="Telefono" required />
                 </div>
-               <input type="submit"  class="btn" value="Registrarse" />
+               <input type="submit" class="btn" value="Registrarse" />
             </form>
          </div>
       </div>
@@ -69,98 +68,15 @@
                Inicia sesión en nuestra página para obtener más información acerca de los eventos que te podemos ofrecer
                </p>
                <button class="btn transparent" id="sign-in-btn">
-                  Sign in
+                  Iniciar Sesión
                </button>
             </div>
             <!-- <img src="img/register.svg" class="image" alt="" /> -->
          </div>
       </div>
    </div>
-
-   <script>
-      const sign_in_btn = document.querySelector("#sign-in-btn");
-      const sign_up_btn = document.querySelector("#sign-up-btn");
-      const container = document.querySelector(".container");
-      const  btn_login = document.getElementById("btn_login")
-
-      btn_login.addEventListener("click",function (e)
-      {
-          login()
-      })
-
-      sign_up_btn.addEventListener("click", () => {
-         container.classList.add("sign-up-mode");
-      });
-
-      sign_in_btn.addEventListener("click", () => {
-         container.classList.remove("sign-up-mode");
-      });
-
-      function login()
-      {
-          let url = "https://roman-company.com/TrailerMovilApiRest/view/login.php";
-          var data = {
-              codigo:"cliente",
-              email:document.getElementById("email_login").value,
-              pass:document.getElementById("pass_login").value
-          }
-
-          //invocamos a la api
-          fetch(url, {
-              method: "POST",
-              body: JSON.stringify(data),
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          })
-              .then(res => res.json())
-              .then(response => {
-                  if (response.status == 200)
-                  {
-                      console.log(response.datos)
-                      insertSessionPHP(response.datos);
-                  }
-                  else{
-                      alert("Credenciales no validas")
-                      // no se pudo realizar
-                  }
-              })
-              .catch(error => console.error('Error:', error));
-      }
-
-
-      function insertSessionPHP(obj)
-      {
-          let url = "./rest/session.php";
-
-          $.ajax({
-              url:url,
-              method:"POST",
-              data:JSON.stringify(obj)
-          }).done(function (datos){
-              var stringtify = JSON.stringify(datos)
-              var json = JSON.parse(stringtify)
-              if (json.status == 200)
-              {
-                  location.href = "./home.php"
-              }else{
-                  alert("ERROR SESSION")
-              }
-          }).fail(function (error){
-              console.log(error)
-              alert(error)
-          })
-
-      }
-   </script>
    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
    <script src="./js/index.js"></script>
-<style>
-    .input-field input{
-        color: whitesmoke;
-    }
-</style>
-
 </body>
 
 </html>
--- js/index.js
+++ js/index.js
@@ -1,56 +1,121 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+const formIniciarSesion = document.getElementById("formIniciarSesion")
+
 getDataClient();
 
-function clientRegister(data){
-    url = "https://roman-company.com/TrailerMovilApiRest/view/cliente.php";
-    fetch(url,{
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers:{
+sign_up_btn.addEventListener("click", () => {
+   container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+   container.classList.remove("sign-up-mode");
+});
+
+formIniciarSesion.addEventListener('submit', async e => {
+   e.preventDefault();
+   console.log('Holaa');
+   const url = "https://roman-company.com/TrailerMovilApiRest/view/login.php";
+   const data = {
+      codigo: "cliente",
+      email: formIniciarSesion.email_login.value,
+      pass: formIniciarSesion.pass_login.value,
+   }
+   try {
+      const request = await fetch(url, {
+         method: "POST",
+         body: JSON.stringify(data),
+         headers: {
             'Content-Type': 'application/json'
-          }
-    }).then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(response => {
-        const Toast = Swal.mixin({
+         }
+      })
+      const response = await request.json();
+      if(response.status == 200){
+         sendDataRest(response.datos)
+      }else{
+         Swal.fire(
+            'Oppss!',
+            'El usuario o la contraseña no son correctas',
+            'error'
+          )
+      }
+   } catch (error) {
+      console.log(error);
+   }
+});
+
+async function sendDataRest(data){
+   const request = await fetch("./rest/session.php", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   });
+   const response = await request.json();
+   if(response.status == 200){
+      location.href = "home.php";
+   }else{
+      Swal.fire(
+         'Oppss!',
+         'Hubo un error interno, intentalo de nuevo',
+         'error'
+       )
+   }
+}
+
+function clientRegister(data) {
+   url = "https://roman-company.com/TrailerMovilApiRest/view/cliente.php";
+   fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => {
+         const Toast = Swal.mixin({
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
             timer: 3000,
             timerProgressBar: true,
             didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
+               toast.addEventListener('mouseenter', Swal.stopTimer)
+               toast.addEventListener('mouseleave', Swal.resumeTimer)
             }
-          })
-          
-          Toast.fire({
+         })
+
+         Toast.fire({
             icon: 'success',
             title: 'Signed in successfully'
-          }) 
-    });
+         })
+      });
 }
 
 function getDataClient() {
-    const  regis_form = document.getElementById("r_formulario");
-    const btn_log = document.getElementById("sign-in-btn");
-    if (!regis_form) return;
+   const regis_form = document.getElementById("r_formulario");
+   const btn_log = document.getElementById("sign-in-btn");
+   if (!regis_form) return;
 
-    regis_form.addEventListener("submit", e=> {
-        e.preventDefault();
-        const datos = {
-            "nombre" : regis_form.r_name.value,
-            "email": regis_form.r_email.value,
-            "password" : regis_form.r_password.value,
-            telefono:  regis_form.r_tel.value,
-            direccion: "",
-            foto: "",
-            dni_ruc: "",
-        }
-        clientRegister(datos);
-        regis_form.reset();
-        btn_log.click();
-        
-    });
+   regis_form.addEventListener("submit", e => {
+      e.preventDefault();
+      const datos = {
+         "nombre": regis_form.r_name.value,
+         "email": regis_form.r_email.value,
+         "password": regis_form.r_password.value,
+         telefono: regis_form.r_tel.value,
+         direccion: "",
+         foto: "",
+         dni_ruc: "",
+      }
+      clientRegister(datos);
+      regis_form.reset();
+      btn_log.click();
+
+   });
 
 }
 
